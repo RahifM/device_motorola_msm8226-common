2c91e2f57547a7f2b693a1242a9fad3164505fb5
ezio84 <brabus84@gmail.com>
2017-05-10 18:40:56 +0200

    Enable real time lockscreen charging info
    Change-Id: I90b9a640264f0805cf0921fbce2e1592bb0caece
    Signed-off-by: RahifM <faizel326@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -117,6 +117,9 @@ TARGET_USERIMAGES_USE_F2FS := true
 # Release tools
 TARGET_RELEASETOOLS_EXTENSIONS := $(VENDOR_PATH)
 
+# Enable real time lockscreen charging current values
+BOARD_GLOBAL_CFLAGS += -DBATTERY_REAL_INFO
+
 # SELinux
 -include device/qcom/sepolicy/sepolicy.mk
 
