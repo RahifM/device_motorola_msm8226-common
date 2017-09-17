db722d48809870e39fbc0b36d9fad65efd67489c
Carlos Arriaga <carlosarriagacm@gmail.com>
2017-09-16 22:54:40 -0500

    msm8226-common: disable fm

--- msm8226.mk
+++ msm8226.mk
@@ -124,9 +124,9 @@ PRODUCT_PACKAGES += \
     libebtc
 
 # FM
-PRODUCT_PACKAGES += \
-    FMRadio \
-    libfmjni
+#PRODUCT_PACKAGES += \
+#    FMRadio \
+#    libfmjni
 
 # GNSS HAL
 PRODUCT_PACKAGES += \
